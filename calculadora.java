f6f28f3956a01c51ea0999ff4a1f6c97f1d62819
xwhiterr <aquinojonathan37@gmail.com>
2023-12-23 09:54:50 -0300

    Subir Calculadora

--- calculadora.java
+++ calculadora.java
@@ -4,7 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 /*
-* @author Stiven Gonzalez
+* @author Walter Aquino
 */
 
 class calculadora implements ActionListener {
